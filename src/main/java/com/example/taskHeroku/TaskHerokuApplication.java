0a1065e3c82c1c912a418f8770f815c898805420
Task1 <thangphph09792@fpt.edu.vn>
2021-11-04 22:13:27 +0700

    Initial commit

---
+++ src/main/java/com/example/taskHeroku/TaskHerokuApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskHeroku;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskHerokuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskHerokuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/taskHeroku/TaskHerokuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskHeroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskHerokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
